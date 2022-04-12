293e8bb55ccca5742a8385e63d5c3bc0dd638b94
JiYongKim <colorful8315@gmail.com>
2022-04-12 10:03:33 +0900

    Back-end Frame

---
+++ src/main/java/wise_SW_life/wiseSWLife/WiseSwLifeApplication.java
@@ -0,0 +1,13 @@
+package wise_SW_life.wiseSWLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseSwLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiseSwLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wise_SW_life/wiseSWLife/WiseSwLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package wise_SW_life.wiseSWLife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseSwLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
