668bf71ff972c9d19ca4d8e79a7723865c1f9937
JiYongKim <colorful8315@gmail.com>
2022-04-13 21:53:36 +0900

    Update Login & SignUp Controller Json type

---
+++ src/main/java/wiseSWlife/wiseSWlife/rest/config/restConfig/RestConfig.java
@@ -0,0 +1,31 @@
+package wiseSWlife.wiseSWlife.rest.config.restConfig;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import wiseSWlife.wiseSWlife.rest.config.interceptor.LoginCheckInterceptor;
+
+@Configuration
+@RequiredArgsConstructor
+public class RestConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/",
+                        "/signUp",
+                        "/login",
+                        "/logout",
+                        "/css/**",
+                        "/images/**",
+                        "/js/**",
+                        "/*.ico",
+                        "/error",
+                        "/webfonts/**");
+
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
+}
---
+++ src/main/java/wiseSWlife/wiseSWlife/rest/session/SessionConst.java
@@ -0,0 +1,5 @@
+package wiseSWlife.wiseSWlife.rest.session;
+
+public abstract class SessionConst {
+    public static final String LOGIN_SESSION_KEY = "Session_Logged_Member";
+}
