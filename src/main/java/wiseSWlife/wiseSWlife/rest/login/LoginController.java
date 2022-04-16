15c5588a460e93f74bae49658a923b371940574d
beombu <sunil1369@bible.ac.kr>
2022-04-17 05:00:58 +0900

    fix : biblebot scraper

--- src/main/java/wiseSWlife/wiseSWlife/rest/login/LoginController.java
+++ src/main/java/wiseSWlife/wiseSWlife/rest/login/LoginController.java
@@ -40,22 +40,40 @@ public class LoginController {
         }
 
         String command = "C:\\Users\\User\\PycharmProjects\\pythonProjectVenv\\venv\\Scripts\\python.exe"; //명령어
-        String arg1 = "C:\\Users\\User\\PycharmProjects\\pythonProjectVenv\\biblebot_scraper\\biblebot-scraper\\test.py";//인지
+        String arg1 = "C:\\Users\\User\\PycharmProjects\\pythonProjectVenv\\Wife_SW_Life\\test.py";//인지
+
+        File testFile = new File(arg1);
+        FileWriter fw = new FileWriter(testFile);
+        fw.write("import asyncio\n" + "\n");
+        fw.write("from biblebot import IntranetAPI\n" + "\n");
+        fw.write("async def main():\n");
+        fw.write("    resp = await IntranetAPI.Login.fetch(\"" + form.getUsername() +"\", \"" + form.getPassword() + "\")\n");
+        fw.write("    result = IntranetAPI.Login.parse(resp)\n");
+        //fw.write("    print(result)\n");
+        fw.write("    cookie = result.data[\"cookies\"]\n");
+        fw.write("    resp = await IntranetAPI.Timetable.fetch(cookies=cookie, semester=20221)\n");
+        fw.write("    result = IntranetAPI.Timetable.parse(resp)\n");
+        fw.write("    print(result)\n");
+        fw.write("asyncio.run(main())");
+        fw.flush();
+        fw.close();
+
         ProcessBuilder builder = new ProcessBuilder(command, arg1);
         Process process = builder.start();
         int exitVal = process.waitFor();//자식 프로세스가 종료될 때까지 기다림
         BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream(), "euc-kr")); // 서브 프로세스가 출력하는 내용을 받기 위해
-        String line;
-        while ((line = br.readLine()) != null) {
-            System.out.println(">>>  " + line); // 표준출력에 쓴다
-        }
+        //String line;
+        String testLine = br.readLine();
+        String test1 = testLine.substring(87,639);
+        System.out.println(test1);
+//        while ((line = br.readLine()) != null) {
+//            System.out.println(">>>  " + line); // 표준출력에 쓴다
+//        }
+        //line.substring(94,610);
         if(exitVal != 0) {
             System.out.println("서브 프로세스가 비정상 종료되었습니다.");
         }
-        //InputStream input = process.getInputStream();//자식 프로세스가 System.out에 출력하는 내용
-        //OutputStream output = process.getOutputStream();//자식 프로세스한테 입력값 전달
 
-        //System.out.println(input);
         //success logic
 
         HttpSession session = request.getSession();
