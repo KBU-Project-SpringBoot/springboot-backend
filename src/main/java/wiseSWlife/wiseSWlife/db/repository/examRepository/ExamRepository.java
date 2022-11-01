99084c1c20d3529c0945beb3a2a2f6fefb7dff8f
beombu <sunil1369@bible.ac.kr>
2022-11-01 18:39:40 +0900

    Feat(Exam_DB) : Exam_TB 기능 목록

---
+++ src/main/java/wiseSWlife/wiseSWlife/db/repository/examRepository/ExamRepository.java
@@ -0,0 +1,13 @@
+package wiseSWlife.wiseSWlife.db.repository.examRepository;
+
+public interface ExamRepository {
+
+    /**
+     * 졸업 시험을 합/불 저장하는 기능
+     */
+
+    /**
+     * 졸업시험 전체를 조회하는 기능
+     */
+
+}
