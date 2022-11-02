70ce0c9fd6ef35e9581d0d3a053c0432f11b0916
beombu <sunil1369@bible.ac.kr>
2022-11-02 13:16:50 +0900

    Feat(Exam_TB) : update Logic

---
+++ src/main/java/wiseSWlife/wiseSWlife/db/entity/examRepository/JdbcExamRepository.java
@@ -0,0 +1,97 @@
+package wiseSWlife.wiseSWlife.db.entity.examRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.stereotype.Repository;
+import wiseSWlife.wiseSWlife.db.repository.examRepository.ExamRepository;
+import wiseSWlife.wiseSWlife.model.graduation.form.ExamForm;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Slf4j
+@Repository
+@Primary
+@RequiredArgsConstructor
+public class JdbcExamRepository implements ExamRepository {
+
+    private final DataSource dataSource;
+
+    @Override
+    public ExamForm save(ExamForm examForm) {
+        return null;
+    }
+
+    @Override
+    public ExamForm update(ExamForm examForm) {
+        String sql = "update Exam_TB set student_id = ?, bible = ?, english = ?, computer = ?, computer2 = ? where student_id = ?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, examForm.getSid());
+            pstmt.setBoolean(2, examForm.getBible());
+            pstmt.setBoolean(3, examForm.getEnglish());
+            pstmt.setBoolean(4, examForm.getComputer());
+            pstmt.setBoolean(5, examForm.getComputer2());
+            pstmt.executeUpdate();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            close(conn, pstmt, rs);
+        }
+
+        return examForm;
+    }
+
+
+    // DB connect
+    private Connection getConnection() {
+        return DataSourceUtils.getConnection(dataSource);
+    }
+
+    // DB close
+    private void close(Connection conn) throws SQLException {
+        DataSourceUtils.releaseConnection(conn, dataSource);
+    }
+
+    // DB close
+    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (conn != null) {
+                close(conn);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/wiseSWlife/wiseSWlife/db/repository/examRepository/ExamRepository.java
+++ src/main/java/wiseSWlife/wiseSWlife/db/repository/examRepository/ExamRepository.java
@@ -1,5 +1,7 @@
 package wiseSWlife.wiseSWlife.db.repository.examRepository;
 
+import wiseSWlife.wiseSWlife.model.graduation.form.ExamForm;
+
 import java.util.Map;
 
 public interface ExamRepository {
@@ -7,11 +9,12 @@ public interface ExamRepository {
     /**
      * 졸업 시험을 합/불 저장하는 기능(첫 스크래핑 시 사용)
      */
-    Map<String, Boolean> save(Map<String, Boolean> examMap);
+    ExamForm save(ExamForm examForm);
 
     /**
-     * 졸업 시험 테이블을 수정하는 기능
+     * 졸업 시험 테이블을 수정하는 기능(스케줄링 시 사용)
      */
+    ExamForm update(ExamForm examForm);
 
     /**
      * 졸업시험 전체를 조회하는 기능
