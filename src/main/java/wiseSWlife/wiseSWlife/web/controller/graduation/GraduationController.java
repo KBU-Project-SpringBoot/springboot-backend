01fc8158247e648b37f53c82947992ef444a4ccc
beombu <sunil1369@bible.ac.kr>
2023-03-25 07:20:53 +0900

    Refactor(graduationController) : 매직 넘버 처리

--- src/main/java/wiseSWlife/wiseSWlife/web/controller/graduation/GraduationController.java
+++ src/main/java/wiseSWlife/wiseSWlife/web/controller/graduation/GraduationController.java
@@ -25,6 +25,16 @@ import java.io.IOException;
 @Controller
 @RequiredArgsConstructor
 public class GraduationController {
+    private final static String MAJOR_BEGIN = "전공기초";
+    private final static String MAJOR_SELECT = "전공선택";
+    private final static String MAJOR_REQUIREMENT = "전공필수";
+    private final static String REFINEMENT_SELECT = "교양선택";
+    private final static String REFINEMENT_REQUIREMENT = "교양필수";
+    public final static String COMPLETION_CREDIT = "이수학점";
+    public final static String AVERAGE_RATING = "평균평점";
+    public final static String BASIC_COMMON_REQUIREMENT = "기초공통필수";
+    public final static String TRANSFER_STUDENT_EXCEPTION_MESSAGE = "편입생은 서비스를 준비중입니다...";
+    public final static String TRANSFER_STUDENT_EXCEPTION_URL = "/";
 
     private final Exam examService;
     private final TotalAcceptanceStatus totalAcceptanceStatusScraping;
@@ -43,8 +53,8 @@ public class GraduationController {
         }
 
         if (sessionForm.getSid().substring(4, 7).equals("070")) {
-            model.addAttribute("exceptionMsg", "편입생은 서비스를 준비중입니다...");
-            model.addAttribute("exceptionUri", "/");
+            model.addAttribute("exceptionMsg", TRANSFER_STUDENT_EXCEPTION_MESSAGE);
+            model.addAttribute("exceptionUri", TRANSFER_STUDENT_EXCEPTION_URL);
             return "home/home";
         }
 
@@ -61,19 +71,19 @@ public class GraduationController {
         ExamForm examForm = examService.exam(sid, intCookie);
         model.addAttribute("examForm", examForm);
 
-        MajorForm majorForm = majorService.getMajorForm(sid, totalAcceptanceStatusTable.getBody().get("전공기초"), totalAcceptanceStatusTable.getBody().get("전공선택"), totalAcceptanceStatusTable.getBody().get("전공필수"));
+        MajorForm majorForm = majorService.getMajorForm(sid, totalAcceptanceStatusTable.getBody().get(MAJOR_BEGIN), totalAcceptanceStatusTable.getBody().get(MAJOR_SELECT), totalAcceptanceStatusTable.getBody().get(MAJOR_REQUIREMENT));
         model.addAttribute("majorForm", majorForm);
 
-        RefinementForm refinementForm = refinementService.getRefinementForm(sid, totalAcceptanceStatusTable.getBody().get("교양선택"), totalAcceptanceStatusTable.getBody().get("교양필수"));
+        RefinementForm refinementForm = refinementService.getRefinementForm(sid, totalAcceptanceStatusTable.getBody().get(REFINEMENT_SELECT), totalAcceptanceStatusTable.getBody().get(REFINEMENT_REQUIREMENT));
         model.addAttribute("refinementForm", refinementForm);
 
-        CreditForm creditForm = creditService.getCredit(sid, Integer.parseInt(totalAcceptanceStatusTable.getSummary().get("이수학점")));
+        CreditForm creditForm = creditService.getCredit(sid, Integer.parseInt(totalAcceptanceStatusTable.getSummary().get(COMPLETION_CREDIT)));
         model.addAttribute("creditForm", creditForm);
 
-        GPAForm gpaForm = gpaService.getGpa(sid, Double.parseDouble(totalAcceptanceStatusTable.getSummary().get("평점평균")));
+        GPAForm gpaForm = gpaService.getGpa(sid, Double.parseDouble(totalAcceptanceStatusTable.getSummary().get(AVERAGE_RATING)));
         model.addAttribute("gpaForm", gpaForm);
 
-        BCRForm bcrForm = basicCommonRequirement.getBCR(sid, totalAcceptanceStatusTable.getBody().get("기초공통필수"), totalAcceptanceStatusTable.getBody().get("교양필수"));
+        BCRForm bcrForm = basicCommonRequirement.getBCR(sid, totalAcceptanceStatusTable.getBody().get(BASIC_COMMON_REQUIREMENT), totalAcceptanceStatusTable.getBody().get(REFINEMENT_REQUIREMENT));
         model.addAttribute("bcrForm", bcrForm);
 
         model.addAttribute("sessionForm", sessionForm);
